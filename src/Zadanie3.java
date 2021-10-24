7838fbb3dfb6b40777cdf02689779a0571052c47
Arkadia <lajluch@gmail.com>
2021-10-24 11:15:26 +0200

    commit

--- src/Zadanie3.java
+++ src/Zadanie3.java
@@ -1,15 +1,15 @@
 public class Zadanie3 {
     public static void main(String[] args) {
-        int a = 2;
-        int b = 5;
+        int a = 1;
+        int b = 4;
         int c = 1;
-        int delta = (int)Math.pow(b, 2) - 4 * (a*c);
+        double delta = Math.pow(b, 2) - 4 * (a*c);
         System.out.println(delta);
-        int x = (-b - (int)Math.sqrt(delta)) / (2*a);
+        double x = (-b - Math.sqrt(delta)) / (2*a);
         System.out.println(x);
-        int x2 = (-b + (int)Math.sqrt(delta)) / (2*a);
+        double x2 = (-b + Math.sqrt(delta)) / (2*a);
         System.out.println(x2);
-        int result = a * (int)Math.pow(x, 2) + b*x + c;
+        double result = a * Math.pow(x, 2) + b*x + c;
         System.out.println(result);
     }
 }
